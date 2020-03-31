abc0befc1eccc9efc7e81a194d2c440d2d708ec6
ArtemKuksenko <kuksenko.artem@gmail.com>
2020-03-31 20:33:00 +0300

    шаблоны условия и циклы

--- app/routes.py
+++ app/routes.py
@@ -11,10 +11,5 @@ def index():
 @app.route('/template')
 def template():
     user = {'username': 'Даша путешственница'}
-    return render_template('template.html', title='Home', user=user)
-
-@app.route('/template')
-def template():
-	# return "Сидим дома на самоизоляции!"
-    user = {'username': 'Даша путешственница'}
-    return render_template('template.html', title='Home', user=user)
+    homebodies = ['По', 'Ляля', 'Тинки-Винки', 'Дипси']
+    return render_template('template.html', title='Home', user=user, homebodies=homebodies)
