9cc09952af68c7650662cf1e12afe5d6f86017fc
lewein <alyonkaigor1107@mail.ru>
2020-04-03 17:29:25 +0300

    add db migrations

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'home-home'
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')  # путь к файлу с нашей базой данных
-    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')  # папка, где мы будем хранить файлы SQLAlchemy-migrate
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #команда для создания папки для миграции - flask db init
 
