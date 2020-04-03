6657926af1fe2a5438c71013648db1a6b4dbc876
lewein <alyonkaigor1107@mail.ru>
2020-04-03 17:39:53 +0300

    add db

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ class Config(object):
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')  # путь к файлу с нашей базой данных
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #команда для создания папки для миграции - flask db init
-
+    #Alembic - инфраструктура миграции
+    #flask db init - команда для создания папки для миграции
+    #flask db migrate -m 'flask table' - команда для переноса БД, которая генерирует автоматические миграции
