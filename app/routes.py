a115cb5a4ebaa0d4520e0ebf48807349187ac1ec
ArtemKuksenko <kuksenko.artem@gmail.com>
2020-04-03 10:41:35 +0300

    шаблоны

--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,23 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+# приметивный базовый пример
 def index():
-    return "Сидим дома на самоизоляции!"
+    return "HELLO HOME!"
 
 @app.route('/template')
+# шаблон, ссылка с примером ниже
+# http://127.0.0.1:5000/template
 def template():
-    user = {'username': 'Даша путешственница'}
+    user = {'username': 'Лягушка путешественница'}
+    homebodies = ['По', 'Ляля', 'Тинки-Винки', 'Дипси']
+    return render_template('template.html', title='Home', user=user, homebodies=homebodies)
+
+
+@app.route('/template/<username>')
+# шблон, ссылка с примером ниже
+# http://127.0.0.1:5000/template/%D0%9B%D1%8F%D0%B3%D1%83%D1%88%D0%BA%D0%B0%20%D0%BF%D1%83%D1%82%D0%B5%D1%88%D0%B5%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%B8%D1%86%D0%B0
+def template_id(username):
+    user = {'username': username}
     homebodies = ['По', 'Ляля', 'Тинки-Винки', 'Дипси']
     return render_template('template.html', title='Home', user=user, homebodies=homebodies)
