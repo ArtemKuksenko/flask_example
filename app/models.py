7d2f13a90fa518630be9f0d352bc8d8d65f979d3
lewein <alyonkaigor1107@mail.ru>
2020-04-03 15:04:32 +0300

    add models

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id_user = db.Column(db.Integer, primary_key=True)
+    name_user = db.Column(db.String(32), index=True, unique=True)
+    post = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User %r' % (self.name_user)
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id_post = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id_user'))
+    text = db.Column(db.String(140))
+    title = db.Column(db.String(32))
+    comment = db.relationship('Comment', backref='comment', lazy='dynamic')
+
+    def __repr__(self):
+        return 'Post %r' % (self.text)
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id_comment = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(64))
+    id_post = db.Column(db.Integer, db.ForeignKey('post.id_post'))
+
+    def __repr__(self):
+        return 'Comment %r' % (self.comment)
